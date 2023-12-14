97c3940895655b083f1c92834023a07245ca52cf
Luwis10 <lwiryanto@student.ciputra.ac.id>
2023-12-14 22:04:49 +0700

    commit message

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -1,20 +1,19 @@
 <x-template>
     <div class="container">
-    @if (count($articles) < 10)
-    <a class="btn btn-success" href="{{ route('article.create') }}">Tambah
-    Artikel</a>
-    @endif
-    @foreach($articles as $article)
-    <div class="card mt-3">
-    <div class="card-body">
-    <h5 class="card-title">{{ $article['title'] }}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">{{
-    $article['date'] }}</h6>
-    <p class="card-text">
-    {{ $article['content'] }}
-    </p>
+        @if (count($articles) < 10)
+            <a class="btn btn-success" href="{{ route('article.create') }}">Tambah Artikel</a>
+        @endif
+
+        @foreach($articles as $article)
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $article['title'] }}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $article['date'] }}</h6>
+                    <p class="card-text">
+                        {{ $article['content'] }}
+                    </p>
+                </div>
+            </div>
+        @endforeach
     </div>
-    </div>
-    @endforeach
-    </div>
-    </x-template>
+</x-template>
