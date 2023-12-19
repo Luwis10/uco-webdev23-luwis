3d0b48fe9b4ebf1978252120bbb0a875585ee7d3
Luwis10 <lwiryanto@student.ciputra.ac.id>
2023-12-19 20:52:54 +0700

    commit message

--- resources/views/article/list.blade.php
+++ resources/views/article/list.blade.php
@@ -1,19 +1,39 @@
-<x-template>
-    <div class="container">
-        @if (count($articles) < 10)
-            <a class="btn btn-success" href="{{ route('article.create') }}">Tambah Artikel</a>
-        @endif
+<!DOCTYPE html>
+<html>
 
-        @foreach($articles as $article)
-            <div class="card mt-3">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $article['title'] }}</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">{{ $article['date'] }}</h6>
-                    <p class="card-text">
-                        {{ $article['content'] }}
-                    </p>
-                </div>
+<head>
+    <title>Artikel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
+</head>
+
+<body><x-template>
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                @if (count($articles) < 10)
+                    <a class="btn btn-success" href="{{ route('article.create') }}">Tambah Artikel</a>
+                @endif
+
+                <a href="{{ route('article_category.list') }}" class="btn btn-primary">Kategori artikel</a>
             </div>
-        @endforeach
-    </div>
-</x-template>
+
+            @foreach ($articles as $article)
+                <div class="card mt-3">
+                    <div class="card-body">
+                        <a href="{{ route('article.single', ['slug' => $article->slug]) }}">
+                            <h5 class="card-title">{{ $article->title }}</h5>
+                        </a>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $article->updated_at }}</h6>
+                        <p class="card-text">
+                            {{ $article->content }}
+                        </p>
+
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </x-template>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,26 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
-Route::get('/', [\App\Http\Controllers\HomeController::class,
-'index'])->name('home')
-;
-Route::controller(\App\Http\Controllers\ArticleController::class)->group(function()
-{
-Route::get('/articles', 'list')->name('article.list');
-Route::match(['get', 'post'], '/articles/create',
-'create')->name('article.create');
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ArticleCategoryController;
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('/articles')->group(function () {
+    Route::get('/', [ArticleController::class, 'list'])->name('article.list');
+    Route::match(['get', 'post'], '/create', [ArticleController::class, 'create'])->name('article.create');
+    Route::get('/{slug}', [ArticleController::class, 'single'])->name('article.single');
+    Route::match(['get', 'post'], '/articles/{id}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+    Route::post('/articles/{id}/delete', [ArticleController::class, 'delete'])->name('article.delete');
 });
+
+Route::prefix('/article_categories')->group(function () {
+    Route::get('/article_categories', [ArticleCategoryController::class, 'index'])->name('article_category.list');
+    Route::get('/article_categories/create', [ArticleCategoryController::class, 'create'])->name('article_category.create');
+    Route::post('/article_categories/create', [ArticleCategoryController::class, 'store']);
+    Route::get('/article_categories/{id}/edit', [ArticleCategoryController::class, 'edit'])->name('article_category.edit');
+    Route::post('/article_categories/{id}/edit', [ArticleCategoryController::class, 'update']);
+    Route::post('/article_categories/{id}/delete', [ArticleCategoryController::class, 'destroy'])->name('article_category.delete');
+});
+
